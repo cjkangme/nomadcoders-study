2d56db688725385e726c9140890f8670d1590032
cjkangme <nureongi0214@gmail.com>
2022-10-13 11:19:30 +0900

    #4.0 ~ #4.4

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,16 @@
-let age = parseInt(prompt('당신의 나이는?'));
+const loginForm = document.querySelector('.login-form');
+const loginInput = loginForm.querySelector('.login-form__input');
+const loginButton = loginForm.querySelector('.login-form__button');
+const greeting = document.querySelector('#greeting');
 
-if ('age >= 19') {
-  alert('You can drink');
-} else if (isNaN(age)) {
-  alert('Please enter Number');
-} else {
-  alert('You cant drink');
+const HIDDEN_CLASSNAME = 'hidden';
+
+function handleLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Welcome, ${username}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
+loginForm.addEventListener('submit', handleLoginSubmit);
