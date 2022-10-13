0293966fe5818d4f94a0984e41cce500756d3f97
cjkangme <nureongi0214@gmail.com>
2022-10-13 11:53:02 +0900

    #4.5 ~ #4.6

--- momentum/app.js
+++ momentum/app.js
@@ -4,13 +4,29 @@ const loginButton = loginForm.querySelector('.login-form__button');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+//function
 
 function handleLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  printGreeting(username);
+}
+
+function printGreeting(username) {
   greeting.innerText = `Welcome, ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', handleLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', handleLoginSubmit);
+} else {
+  printGreeting(savedUsername);
+}
