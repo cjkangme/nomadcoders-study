581ee16a31926c6fc3d3a375b506c52974e051e6
cjkangme <nureongi0214@gmail.com>
2022-10-18 16:18:27 +0900

    #7.1 Weather API

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = '944c738ce4a56b8e700b1a95bd3fc1af';
+
 function onGeoSuccess(location) {
   let lat = location.coords.latitude;
   let long = location.coords.longitude;
-  console.log('You live in', lat, long);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric&lang=kr`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const temp = document.querySelector('.weather__temp');
+      const name = document.querySelector('.weather__name');
+      const icon = document.querySelector('.weather__icon');
+      temp.innerText = `${data.main.temp}℃`;
+      name.innerText = data.name;
+      icon.setAttribute('src', `assets/icons/${data.weather[0].icon}.png`);
+    });
 }
 
 function onGeoError() {
