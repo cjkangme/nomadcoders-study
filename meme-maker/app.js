257577da7392eb14ff052da13ba4c7f9e837df2b
cjkangme <nureongi0214@gmail.com>
2022-10-19 20:28:49 +0900

    #1.2 Paths

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -4,4 +4,30 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200);
+// ctx.fillRect(200, 200, 20, 200);
+// ctx.fillRect(400, 200, 20, 200);
+// ctx.lineWidth = 2;
+// ctx.strokeRect(285, 300, 50, 100);
+// ctx.fillRect(200, 200, 200, 20);
+
+// ctx.beginPath();
+// ctx.moveTo(200, 200);
+// ctx.lineTo(310, 100);
+// ctx.lineTo(420, 200);
+// ctx.lineWidth = 2;
+// ctx.fill();
+
+ctx.beginPath();
+ctx.fillRect(185, 200, 15, 100);
+ctx.fillRect(300, 200, 15, 100);
+ctx.strokeRect(220, 200, 60, 100);
+ctx.fillRect(210, 315, 20, 100);
+ctx.fillRect(270, 315, 20, 100);
+ctx.arc(250, 160, 30, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(240, 160, 5, -0.25 * Math.PI, 1.25 * Math.PI);
+ctx.arc(260, 160, 5, -0.25 * Math.PI, 1.25 * Math.PI);
+ctx.fillStyle = '#fff';
+ctx.fill();
