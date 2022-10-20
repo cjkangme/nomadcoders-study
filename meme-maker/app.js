0c15b875ab50781aabda5615021ecc2f3038e1fd
cjkangme <nureongi0214@gmail.com>
2022-10-20 14:20:24 +0900

    #2.1 Mouse Painting

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,33 +1,45 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
+const colors = [
+  '#C7395F',
+  '#DED4E8',
+  '#E8BA40',
+  '#EDCBD2',
+  '#80C4B7',
+  '#E3856B',
+  '#3B5BA5',
+  '#BBCB50',
+  '#4AAFD5',
+  '#E7A339',
+  '#552619',
+  '#31473A',
+];
+
 canvas.width = 800;
 canvas.height = 800;
 
-// ctx.fillRect(200, 200, 20, 200);
-// ctx.fillRect(400, 200, 20, 200);
-// ctx.lineWidth = 2;
-// ctx.strokeRect(285, 300, 50, 100);
-// ctx.fillRect(200, 200, 200, 20);
+ctx.lineWidht = 2;
 
-// ctx.beginPath();
-// ctx.moveTo(200, 200);
-// ctx.lineTo(310, 100);
-// ctx.lineTo(420, 200);
-// ctx.lineWidth = 2;
-// ctx.fill();
+let isPainting = false;
 
-ctx.beginPath();
-ctx.fillRect(185, 200, 15, 100);
-ctx.fillRect(300, 200, 15, 100);
-ctx.strokeRect(220, 200, 60, 100);
-ctx.fillRect(210, 315, 20, 100);
-ctx.fillRect(270, 315, 20, 100);
-ctx.arc(250, 160, 30, 0, 2 * Math.PI);
-ctx.fill();
+function drawMousemove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 5, -0.25 * Math.PI, 1.25 * Math.PI);
-ctx.arc(260, 160, 5, -0.25 * Math.PI, 1.25 * Math.PI);
-ctx.fillStyle = '#fff';
-ctx.fill();
+function drawMousedown(event) {
+  isPainting = true;
+}
+
+function cancleDrawing(event) {
+  isPainting = false;
+}
+
+canvas.addEventListener('mousemove', drawMousemove);
+canvas.addEventListener('mousedown', drawMousedown);
+canvas.addEventListener('mouseup', cancleDrawing);
+canvas.addEventListener('mouseout', cancleDrawing);
